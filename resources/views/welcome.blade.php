e36a86f2f72ff1fc2d66eff8f12054b6e89a7005
RAFAEL TORICES <torices.rafael@gmail.com>
2021-07-02 14:07:27 +0200

    Modificado el Welcome - simplificado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,17 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 
 <body>
-    <div class="container-fluid" style="margin-top:20px;">
+    <div class="row align-items-center justify-content-center vh-100" style="font-family:'Poppins', sans-serif;">
         @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
@@ -29,11 +33,8 @@
             @endauth
         </div>
         @endif
-        <div align="center">
-            <hr>
-            <h5 class="card-title">Application Web ApiRest</h5>
-            <p class="card-text">TORICES - Servicio ApiRest de Pruebas</p>
-            <hr>
+        <div class="text-center">
+            <h5 class="card-title">TORICES Apps</h5>
         </div>
     </div>
 </body>
